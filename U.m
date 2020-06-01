3f773b2af77d16ffbd080c74f53fb56c2a93d7e8
Adalberto <valsecchi.adalberto@gmail.com>
2020-06-01 18:00:43 +0200

    Commenti aggiunti

--- U.m
+++ U.m
@@ -7,9 +7,6 @@ pm = 0.1; (* probabilità di avere una mutazione *)
 (* Parola desiderata *)
 goal = {u, n, i, v, e, r, s, a, l, e};
 
-stack = {n, s};
-table = {v, u, i, l, e, a, e, r};
-
 (* contapassi *)
 contapassi = 0;
 spostaLettere = 0;
@@ -18,54 +15,45 @@ spostaLettere = 0;
 
 CS := Module[{result},
 
-		contapassi += 1;
-
-		If[ Length[stack] > 0,
-
-			result = stack[[ Length[stack] ]],
-
-			result = "Null"
-		];
-
-		result
-
+	contapassi += 1;
+	If[ Length[stack] > 0,
+		result = stack[[ Length[stack] ]],
+		result = "Null"
 	];
+
+	result
+
+];
 
 
 TB := Module[ {index, result},
 
-		contapassi += 1;
-		index = Indice[stack, goal];
+	contapassi += 1;
+	index = Indice[stack, goal];
 
-		If[ index === 0,
+	If[ index === 0,
+		result = "Null",
+		result = goal[[index]];
+	];		
 
-			result = "Null",
+	result
 
-			result = goal[[index]];
-
-		];		
-
-		result
-
-	];
+];
 
 
 NN := Module[ {index, result},
 
-		contapassi += 1;
-		index = Indice[stack, goal];
+	contapassi += 1;
+	index = Indice[stack, goal];
 
-		If[ index === Length[goal],
-
-			result = "Null",
-
-			result = goal[[ index + 1 ]];
-
-		];
-
-		result
-
+	If[ index === Length[goal],
+		result = "Null",
+		result = goal[[ index + 1 ]];
 	];
+
+	result
+
+];
 
 
 (* Operazioni *)
@@ -74,40 +62,38 @@ NN := Module[ {index, result},
 
 MS[block_] := Module[{result},
 
-		contapassi += 1;
+	contapassi += 1;
 
-		If[ MemberQ[table, block],
-			spostaLettere += 1;
-			AppendTo[stack, block];
-			table = Delete[table, Position[table, block][[1]]];
-			result = block,
+	If[ MemberQ[table, block],
+		spostaLettere += 1;
+		AppendTo[stack, block];
+		table = Delete[table, Position[table, block][[1]]];
+		result = block,
 
-			result = "Null";
-
-		];
-
-		result
-
+		result = "Null";
 	];
+
+	result
+
+];
 
 
 MT[block_] := Module[{result},
 
-		contapassi += 1;
+	contapassi += 1;
 
-		If[ MemberQ[stack, block],
-			spostaLettere += 1;
-			table = Append[ table, stack[[-1]] ];
-			stack = Delete[ stack, -1 ];
-			result = block,
+	If[ MemberQ[stack, block],
+		spostaLettere += 1;
+		table = Append[ table, stack[[-1]] ];
+		stack = Delete[ stack, -1 ];
+		result = block,
 
-			result = "Null"
-
-		];
-
-		result
-
+		result = "Null"
 	];
+
+	result
+
+];
 
 
 (* Operazioni logiche *)
@@ -116,172 +102,237 @@ SetAttributes[DU, HoldAll];
 
 DU[expr1_, expr2_] := Module[{contapassiDU},
 
-		contapassi += 1;
-		contapassiDU = 0; 
+	contapassi += 1;
+	contapassiDU = 0; 
 
-		While[ReleaseHold[expr2] === "Null" && contapassiDU <= 3*Length[goal] && contapassiDUtot <= 15 * Length[goal],
-			ReleaseHold[expr1];
-			contapassiDU += 1;
-			contapassiDUtot += 1;
-		];
-
-		If[ contapassiDU > 3*Length[goal],
-			loop = 1; (* se = 1 vuol dire che c'è stato un loop *)
-		];
-
-		ReleaseHold[expr2] (* DU restituisce True se non "sfora" 3*N
-                              passi, altrimenti restituisce Null *)
-
+	While[ReleaseHold[expr2] === "Null" && contapassiDU <= 3*Length[goal] && contapassiDUtot <= 15 * Length[goal],
+		ReleaseHold[expr1];
+		contapassiDU += 1;
+		contapassiDUtot += 1;
 	];
+
+	If[ contapassiDU > 3*Length[goal],
+		loop = 1; (* se = 1 vuol dire che c'è stato un loop *)
+	];
+
+	ReleaseHold[expr2] (* DU restituisce True se non "sfora" 3*N passi, altrimenti restituisce Null *)
+
+];
 
 
 NOT[expr1_] := Module[{result},
 
-		contapassi += 1;
+	contapassi += 1;
 
-		If[ expr1 === "Null",
-
-			result = True,
-
-			result = "Null"
-
-		];
-
-		result
-
+	If[ expr1 === "Null",
+		result = True,
+		result = "Null"
 	];
+
+	result
+
+];
 
 
 EQ[expr1_, expr2_] := Module[{result},
 
-		contapassi += 1;
+	contapassi += 1;
 
-		If[ expr1 === expr2,
-
-			result = True,
-
-			result = "Null"
-
-		];
-
-		result
-
+	If[ expr1 === expr2,
+		result = True,
+		result = "Null"
 	];
 
+	result
+
+];
 
 
-(*********************************** ALGORITMO GENETICO ******************************)
-
-(* Costruzione di un individuo *)
+(********************** QUI INIZIA LA PARTE DI ALGORITMO GENETICO **********************)
 
 (* Liste di comandi necessarie per la generazione di un individuo *)
 comandi = {xEQ[arg, arg], xNOT[arg], xDU[arg, arg], xMT[arg], xMS[arg], xNN, xTB, xCS}; (*per EQ, NOT e 1° argomento di DU*)
 comandiTF = {xEQ[arg, arg], xNOT[arg], xDU[arg, arg]}; (* per 2° argomento di DU *)
 sensori = {xCS, xTB, xNN}; (* per MS, MT*)
 
-
 ii = Hold[xEQ[arg, arg]]; (* individuo iniziale *)
 
-Individuo[] := Module[{Pos, pos, com, i, individuo},
-
-		individuo = ii;
-
-		While[MemberQ[individuo, arg, Infinity] && Depth[individuo] <= 5,
-			Pos = Position[individuo, arg];
-
-			For[i=1, i <= Length[Pos], i++,
-				pos = Pos[[i]];
-				pos[[-1]] = 0; (* In questo modo leggo la Head di arg *)
-
-				(* Decidiamo da che lista di comandi estraiamo *)
-				If[ MemberQ[{xEQ, xNOT}, Part[individuo, Sequence @@ pos]],
-					com = comandi,
-					If[ MemberQ[{xMS, xMT}, Part[individuo, Sequence @@ pos]],
-						com = sensori,
-						(* Non rimane che il DU: 1° o 2° argomento? *)
-						If[Pos[[i]][[Length[Pos[[i]]]]] === 1,
-							com = comandi,
-							com = comandiTF;
-						];
-					];
-				];
-
-				individuo[[Sequence @@ Pos[[i]]]] = com[[Random[Integer, {1, Length[com]}]]];
-
-				];
-
-		];
-
-		If[ MemberQ[individuo, arg, Infinity],
-			individuo = Individuo[]
-		];
-
-		individuo
-
-	];
-
-
-(* Costruzione di un individuo a partire da un altro individuo già formato,
-   ma con un arg da qualche parte *)
-
+(* Costruzione di un individuo *)
 Individuo[ind_] := Module[{Pos, pos, com, i, individuo},
 
-		individuo = ind;
+	individuo = ind;
 
-		While[ MemberQ[individuo, arg, Infinity] && Depth[individuo] <= 3 + Depth[ind],
-			
-			Pos = Position[individuo, arg];
+	While[MemberQ[individuo, arg, Infinity] && Depth[individuo] <= 3 + Depth[ind],
+	
+		Pos = Position[individuo, arg];
 
-			For[i=1, i <= Length[Pos], i++,
-				pos = Pos[[i]];
-				pos[[-1]] = 0; (* In questo modo leggo la Head di arg *)
+		For[i=1, i <= Length[Pos], i++,
+			pos = Pos[[i]];
+			pos[[-1]] = 0; (* In questo modo leggo la Head di arg *)
 
-				(* Decidiamo da che lista di comandi estraiamo *)
-				If[ MemberQ[{xEQ, xNOT}, Part[individuo, Sequence @@ pos]],
-					com = comandi,
-					If[ MemberQ[{xMS, xMT}, Part[individuo, Sequence @@ pos]],
-						com = sensori,
-						(* Non rimane che il DU: 1° o 2° argomento? *)
-						If[Pos[[i]][[Length[Pos[[i]]]]] === 1,
-							com = comandi,
-							com = comandiTF;
-						];
+			(* Decidiamo da che lista di comandi estraiamo *)
+			If[ MemberQ[{xEQ, xNOT}, Part[individuo, Sequence @@ pos]],
+				com = comandi,
+				If[ MemberQ[{xMS, xMT}, Part[individuo, Sequence @@ pos]],
+					com = sensori,
+					(* Non rimane che il DU: 1° o 2° argomento? *)
+					If[Pos[[i]][[Length[Pos[[i]]]]] === 1,
+						com = comandi,
+						com = comandiTF;
 					];
 				];
-
-				individuo[[Sequence @@ Pos[[i]]]] = com[[Random[Integer, {1, Length[com]}]]];
 			];
-
+			
+			(* Peschiamo un comando casuale tra quelli sintatticamente compatibili *)
+			individuo[[Sequence @@ Pos[[i]]]] = com[[Random[Integer, {1, Length[com]}]]];
 		];
-
-		If[ MemberQ[individuo, arg, Infinity],
-			individuo = Individuo[ind];
-		];
-
-		individuo
-
 	];
 
+	(* Se alla fine del While precedente sono ancora presenti degli arg allora ri-genero un individuo *)
+	If[ MemberQ[individuo, arg, Infinity],
+		individuo = Individuo[ind]
+	];
 
+	individuo
+
+];
+
+
+(* Genero una popolazione di num individui *)
+Popolazione[num_] := Table[Individuo[ii], {i, num}];
+
+
+(* Per eseguire effettivamente un individuo dobbiamo rimuovere le x prima di ogni comando *)
 EseguiIndividuo[individuo_] := Module[{result},
 
-		result = individuo /. {xCS->Hold[CS], xTB->Hold[TB], xNN->Hold[NN], xNOT->NOT,
-                               xMT->MT, xMS->MS, xEQ->EQ, xDU->DU};
+	contapassiDUtot = 0;
 
-		result = Map[ReleaseHold, result, {0, Infinity}];
+	result = individuo /. {xCS->Hold[CS], xTB->Hold[TB], xNN->Hold[NN], xNOT->NOT,
+                           xMT->MT, xMS->MS, xEQ->EQ, xDU->DU};
+
+	result = Map[ReleaseHold, result, {0, Infinity}];
+	
+	result
+];
+
+
+(* Lista di tutti i possibili comandi *)
+tutto = {xEQ, xNOT, xDU, xMT, xMS, xCS, xTB, xNN};
+
+(* Crossover tra una coppia di individui *)	
+Crossover[coppia_] := Module[{ind1, ind2, temp1, crossoverOK, Rand, Pos1, swap, pos1, Headswap, blacklistHead, temp, togliHead, Pos2},
+
+	ind1 = coppia[[1]];
+	ind2 = coppia[[2]];
+	
+	If[Random[] < pc,
+
+		(* Lista delle posizioni di tutti i comandi nel primo individuo *)
+		temp1 = Flatten[Table[Position[ind1, tutto[[i]]], {i,1,Length[tutto]}], 1] /. {x___,0} -> {x};
+		temp1 = DeleteCases[ temp1, {1}]; (* Non può pescare tutto l'individuo (magari toglierò questo comando)*)		
 		
-		result
+		(* Variabile booleana che mi dice se è possibile effettuare il crossover o meno *)
+		crossoverOK = False;
+
+		While[ crossoverOK === False && Length[temp1] > 0,
+
+			(* Pesco una delle possibili componenti dell'individuo 1 e la cancello dalla lista temp1,
+			   così se ripeto la procedura non mi capiterà di pescarla di nuovo *)
+			Rand = Random[Integer, {1, Length[temp1]}];
+			Pos1 = temp1[[ Rand ]];
+			temp1 = Delete[temp1, Rand];
+
+			swap = ind1[[ Sequence @@ Pos1 ]];
+
+			(* Headswap è il comando che prende in input swap *)
+			pos1 = Pos1;
+			pos1[[-1]] = 0;
+			Headswap = ind1[[ Sequence @@ pos1 ]];
+
+			(* Creo una lista dei comandi non compatibili con Headswap (i comandi che non si possono dare in input a Headswap) *)
+			Which[ MatchQ[Headswap, xNOT | xEQ], blacklistHead = {},
+				   MatchQ[Headswap, xMT | xMS], blacklistHead = {xDU, xNOT, xEQ, xMS, xMT},
+				   MatchQ[ Headswap, xDU], 
+						If[ Pos1[[-1]] === 1, 
+							blacklistHead = {},
+							blacklistHead = {xMS, xMT, xNN, xCS, xTB};
+						];
+			];
+			
+			(* Butto via i pezzi di programma non compatibili *)
+			temp = Flatten[Table[Position[ind2, tutto[[i]]], {i,1,Length[tutto]}], 1] /. {x___,0} -> {x};
+			
+			temp = DeleteCases[ temp, {1}]; (* Non può pescare tutto l'individuo (magari toglierò questo comando)*)
+			
+			togliHead = Flatten[Table[Position[ind2, blacklistHead[[i]]], {i,1,Length[blacklistHead]}], 1] /. {x___,0}->{x};
+			temp = DeleteCases[ temp, Alternatives @@ togliHead]; 
+			
+			(* E ora butto via i pezzi di programma non compatibili con swap (i comandi a cui non posso dare come input swap) *)
+			If[ MatchQ[ Head[swap], Symbol],
+				temp = DeleteCases[temp, Alternatives @@ Position[ind2, xDU] /. {x___,0}->{x,2}],
+				(* If MatchQ[ Head[swap], xEQ | xNOT | xDU ] *)
+				temp = DeleteCases[temp, Alternatives @@ Position[ind2, xMS | xMT] /. {x___,0}->{x,1}];
+			];
+
+			(* Ora peschiamo tra le posizioni dei comandi rimanenti (se ce ne sono!) una posizione *)
+			If[ Length[temp] > 0,
+				Pos2 = temp[[ Random[Integer, {1, Length[temp]}] ]];
+				crossoverOK = True;
+			];
+		];
+
+		(* Se abbiamo trovato uno scambio possibile lo effettuiamo! *)
+		If[crossoverOK === True,
+			ind1[[ Sequence @@ Pos1 ]] = ind2[[ Sequence @@ Pos2 ]];
+			ind2[[ Sequence @@ Pos2 ]] = swap;
+		];
+
 	];
 
+	{ind1, ind2}
+];
 
 
-Popolazione[num_] := Module[{pop},
+Mutazione[ind_] := Module[{individuo, temp, Pos},
 
-		pop = Table[Individuo[], {i, num}];
+	individuo = ind;
 
-		pop
+	If[ Random[] < pm,
+
+		temp = Flatten[Table[Position[individuo, tutto[[i]]], {i,1,Length[tutto]}], 1];
+		temp = ReplaceAll[temp, {x___,0} -> {x} ];
+		
+		(* Scelgo una posizione casuale nell'individuo e scrivo "arg" al posto di quello che c'era scritto prima.
+		   In questo modo abbiamo scelgo una posizione casuale da cui far partire la mutazione! *)
+		Pos = temp[[ Random[Integer, {2, Length[temp]}] ]];
+		individuo[[ Sequence @@ Pos ]] = arg;
+
+		individuo = Individuo[individuo];
 
 	];
+
+	individuo
+	
+];
+
+
+ricombina[popolazione_] := Module[{temp},
+
+	(* Divido la popolazione in coppie *)
+	temp = Partition[popolazione,2];
+	
+	(* Applico il crossover alle coppie di individui *)
+	figli = Map[Crossover, temp];
+	
+	(* "Sciolgo" le coppie *)
+	figli = Flatten[figli, 1];
+	
+	(* Applico la mutazione ad ogni individuo figlio *)
+	figli = Map[Mutazione, figli];
+	
+	figli
+
+];
 
 
 Fitness[individuo_] := Module[{fitness, stackIniziale, istack},
@@ -308,7 +359,6 @@ Fitness[individuo_] := Module[{fitness, stackIniziale, istack},
 			
 			(* Inizializzo le variabili per calcolare la fitness *)			
 			contapassi = 0;
-			contapassiDUtot = 0;
 			spostaLettere = 0;
 			
 			fitness = 0;
@@ -357,169 +407,71 @@ Fitness[individuo_] := Module[{fitness, stackIniziale, istack},
 	];
 	
 	
-		If[ fitnesstot > fitnessmax,
-			fitnessmax = fitnesstot;
-			imax = ipop;
-		];
-
-		fitnesstot
-
+	If[ fitnesstot > fitnessmax,
+		fitnessmax = fitnesstot;
+		imax = ipop;
 	];
+	
+	ipop++;
+	
+	fitnesstot
+
+];
+	
 
 voti[popolazione_] := Map[Fitness, popolazione];
 
-generazione[popolazione_] := Module[{temp, r},
-
-		votipop = voti[popolazione];
-		intervallo = suddivido[votipop, FitnessProportionate];
-		genitori = Table[
-				r = Random[];
-				indice = Count[intervallo, x_ /; x<r] + 1; 
-				popolazione[[indice]],
-				{i,1,Npop}
-			];
-
-		figli = ricombina[genitori];
-		
-		figli
-	];
 
 suddivido[voti_, criterio_] := Module[{temp},
-		Which[criterio === FitnessProportionate,
-			totalevoti = Plus @@ voti;
-			frazioni = voti/totalevoti;
-			suddivisione = Table[Sum[frazioni[[j]], {j,1,i}] , {i,1,Npop}], 
-			True, (* caso di default *)
-			Print["criterio non definito"]; Abort[]
-	
+
+	Which[criterio === FitnessProportionate,
+		totalevoti = Plus @@ voti;
+		frazioni = voti/totalevoti;
+		suddivisione = Table[Sum[frazioni[[j]], {j,1,i}] , {i,1,Npop}], 
+		True, (* caso di default *)
+		Print["criterio non definito"]; Abort[]
+
+	];
+
+	suddivisione
+
+];
+
+
+(* Genero una popolazione di figli a partire da una popolazione *)
+generazione[popolazione_] := Module[{temp, r},
+
+	votipop = voti[popolazione];
+	intervallo = suddivido[votipop, FitnessProportionate];
+	genitori = Table[
+			r = Random[];
+			indice = Count[intervallo, x_ /; x<r] + 1; 
+			popolazione[[indice]],
+			{i,1,Npop}
 		];
 
-		suddivisione
-
-	];
-
-
-(* Crossover *)
-
-tutto = {xEQ, xNOT, xDU, xMT, xMS, xCS, xTB, xNN}; (* tutti i possibili comandi *)
+	figli = ricombina[genitori];
 	
-Crossover[coppia_] := Module[{ind1, ind2, temp1, crossoverOK, Rand, Pos1, swap, pos1, Headswap, blacklistHead, temp, togliHead, togli, Pos2, result},
-
-		ind1 = coppia[[1]];
-		ind2 = coppia[[2]];
-		
-		If[Random[] < pc,
-
-			temp1 = Flatten[Table[Position[ind1, tutto[[i]]], {i,1,Length[tutto]}], 1] /. {x___,0} -> {x};
-			temp1 = DeleteCases[ temp1, {1}]; (* Non può pescare tutto l'individuo (magari toglierò questo comando)*)		
-			crossoverOK = False;
-
-			While[ crossoverOK === False && Length[temp1] > 0,
-
-				Rand = Random[Integer, {1, Length[temp1]}];
-				Pos1 = temp1[[ Rand ]];
-				temp1 = Delete[temp1, Rand];
-
-				swap = ind1[[ Sequence @@ Pos1 ]];
-
-				pos1 = Pos1;
-				pos1[[-1]] = 0;
-				(* Headswap è il comando che prende in input swap *)
-				Headswap = ind1[[ Sequence @@ pos1 ]];
-
-				Which[ MatchQ[Headswap, xNOT | xEQ], blacklistHead = {},
-					   MatchQ[Headswap, xMT | xMS], blacklistHead = {xDU, xNOT, xEQ, xMS, xMT},
-					   MatchQ[ Headswap, xDU], 
-							If[ Pos1[[-1]] === 1, 
-								blacklistHead = {},
-								blacklistHead = {xMS, xMT, xNN, xCS, xTB};
-							];
-				];
-				
-				(* Qui butto via i pezzi di programma non compatibili *)
-				(* DA OTTIMIZZARE *)
-				temp = Flatten[Table[Position[ind2, tutto[[i]]], {i,1,Length[tutto]}], 1] /. {x___,0} -> {x};
-				
-				temp = DeleteCases[ temp, {1}]; (* Non può pescare tutto l'individuo (magari toglierò questo comando)*)
-				
-				togliHead = Flatten[Table[Position[ind2, blacklistHead[[i]]], {i,1,Length[blacklistHead]}], 1] /. {x___,0}->{x};
-				temp = DeleteCases[ temp, Alternatives @@ togliHead]; 
-				
-				If[ MatchQ[ Head[swap], Symbol],
-					temp = DeleteCases[temp, Alternatives @@ Position[ind2, xDU] /. {x___,0}->{x,2}],
-					(* If MatchQ[ Head[swap], xEQ | xNOT | xDU ] *)
-					temp = DeleteCases[temp, Alternatives @@ Position[ind2, xMS | xMT] /. {x___,0}->{x,1}];
-				];
-				(* Fine di: butto via i pezzi di programma non compatibili *)
-
-				If[ Length[temp] > 0,
-					Pos2 = temp[[ Random[Integer, {1, Length[temp]}] ]];
-					crossoverOK = True;
-				];
-			];
-
-			If[crossoverOK === True,
-				ind1[[ Sequence @@ Pos1 ]] = ind2[[ Sequence @@ Pos2 ]];
-				ind2[[ Sequence @@ Pos2 ]] = swap;
-			];
-	
-		];
-
-		result = {ind1, ind2}
-	];
-
-
-Mutazione[ind_] := Module[{individuo, temp, Pos},
-
-		individuo = ind;
-
-		If[ Random[] < pm,
-
-			temp = Flatten[Table[Position[individuo, tutto[[i]]], {i,1,Length[tutto]}], 1];
-			temp = ReplaceAll[temp, {x___,0} -> {x} ];
-			
-			Pos = temp[[ Random[Integer, {2, Length[temp]}] ]];
-
-			individuo[[ Sequence @@ Pos ]] = arg;
-
-			individuo = Individuo[individuo];
-
-		];
-
-			individuo
-	];
-
-
-ricombina[popolazione_] := Module[{temp},
-
-		temp = Partition[popolazione,2];
-		
-		figli = Map[Crossover, temp];
-		
-		figli = Flatten[figli, 1];
-		figli = Map[Mutazione, figli];
-		
-		figli
-
-	];
+	figli
+];
 
 
 run := Module[{temp, igen},
 
+		StacksAndTables;
+		soluzione = "Null";
 		trovato = 0;
 		
-		StacksAndTables;
+		(* Genero una nuova popolazione ogni volta che lancio run *)
+		pop = Popolazione[Npop]; 
 		
-		soluzione = "Null";
-		
-		pop = Popolazione[Npop]; (* pop è una nuova popolazione ogni volta che lancio run *)
 		
 		For[igen=0, igen<Ngen, igen++,
 			Print["Generazione: ", igen+1];
 			
 			ipop = 1;
 			fitnessmax = 0;
-			
+
 			pop = generazione[pop];
 			Print["Fitness massima = ", N[fitnessmax], "\n"];
 			
@@ -537,19 +489,18 @@ run := Module[{temp, igen},
 
 stackRand := Module[{lenstack},
 
-	(* Genero la condizione iniziale in modo casuale estraendo lettere da goal *)
+	(* Genero la stack (e table) in modo casuale estraendo lettere da goal *)
 	lenstack = Random[Integer, {0, Length[goal]}];
 	stack = RandomSample[goal, lenstack];
-	(*Print["stack iniziale = ", stack];*)
 	table = DeleteCasesOnce[goal, stack];
-	(*Print["table iniziale = ", table];*)
 
 ];
 
 StacksAndTables := Module[{is},
 
 	(*stacks = {{}, {u,n,l}, {r, n, i, e, u, v, a, l, s}, {u}, {s, v, r, u, a}};
-	tables = Table[DeleteCasesOnce[goal, stacks[[is]]], {is,1,5}];*)
+	tables = Table[DeleteCasesOnce[goal, stacks[[is]]], {is, 1, 5}];*)
+	
 	stacks = {};
 	tables = {};
 	For[is = 1, is <= 5, is++,
@@ -564,13 +515,15 @@ StacksAndTables := Module[{is},
 
 Try := Module[{},
 
-	contapassiDUtot = 0;
 	stackRand;
 	Print["stack iniziale = ", stack];
+	
 	EseguiIndividuo[soluzione];
+	
 	Print["stack finale = ", stack];
 	
 ];
+
 
 (* Funzioni aggiuntive *)
 
